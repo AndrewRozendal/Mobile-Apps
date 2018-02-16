7bbffc7a0308c057e23f3736f5a67313949e82db
Andrew Rozendal <ascrozendal@gmail.com>
2018-02-16 11:35:26 -0800

    refactored and commented for clarity

--- TemperatureConverter/TemperatureConverter/ViewController.swift
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     //MARK: Properties
+    // Identifies user conversion choice
     @IBOutlet weak var desiredConversionChoice: UISegmentedControl!
+    // Field for user value to convert
     @IBOutlet weak var valueToConvertField: UITextField!
+    // Label to hold end result
     @IBOutlet weak var resultField: UILabel!
     
     //Mark: Delegate Methods
@@ -25,19 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: Actions
     
+    // Called when convert button pressed
+    // Converts the value in the input field according to the formula associated with the segment selection.
+    // Outputs the result to the result field
     @IBAction func convertValueBtn(_ sender: Any) {
-        if let value = self.valueToConvertField.text {
-            if self.desiredConversionChoice.selectedSegmentIndex == 0 {
-                // Conversion 1
-                self.resultField.text = "Conversion 1 with \(value)"
-            } else {
-                // Conversion 2
-                self.resultField.text = "Conversion 2 with \(value)"
-            }
-        } else {
-            //Text Field was nil
+        guard let textValue = self.valueToConvertField.text else {
+            // valueToConvertField was nil
             self.resultField.text = "N/A"
+            return
+        }
+        
+        guard let value = Double(textValue) else {
+            // textValue could not be converted to Double
+            self.resultField.text = "N/A"
+            return
+        }
+        
+        if self.desiredConversionChoice.selectedSegmentIndex == 0 {
+            // Conversion 1
+            self.resultField.text = "Conversion 1 with \(value)"
+        } else {
+            // Conversion 2
+            self.resultField.text = "Conversion 2 with \(value)"
         }
     }
     
