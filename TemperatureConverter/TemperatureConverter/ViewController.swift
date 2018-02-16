daa7cce3933c07e22bf4e42bb53f5a4821394896
Andrew Rozendal <ascrozendal@gmail.com>
2018-02-16 11:21:16 -0800

    conversion page ui elements acting correctly, fire placeholders

--- TemperatureConverter/TemperatureConverter/ViewController.swift
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var desiredConversionChoice: UISegmentedControl!
+    @IBOutlet weak var valueToConvertField: UITextField!
+    @IBOutlet weak var resultField: UILabel!
+    
+    //Mark: Delegate Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func convertValueBtn(_ sender: Any) {
+        if let value = self.valueToConvertField.text {
+            if self.desiredConversionChoice.selectedSegmentIndex == 0 {
+                // Conversion 1
+                self.resultField.text = "Conversion 1 with \(value)"
+            } else {
+                // Conversion 2
+                self.resultField.text = "Conversion 2 with \(value)"
+            }
+        } else {
+            //Text Field was nil
+            self.resultField.text = "N/A"
+        }
+    }
+    
 
 }
 
