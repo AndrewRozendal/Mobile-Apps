e4ad6eb36dc6965bff083096e4c89d8e28874526
Andrew Rozendal <ascrozendal@gmail.com>
2018-03-02 11:06:46 -0800

    added a couple more test movies to ensure scrolling works correctly

--- Movie Database/Movie Database/MovieCollection.swift
+++ Movie Database/Movie Database/MovieCollection.swift
@@ -43,7 +43,9 @@ class MovieCollection {
             2: Movie(id: 2, title: "Saving Private Ryan", genres: [Genres.Action], actors: ["Tom Hanks"], rating: 5, isFavourite: true, comments: "A classic", image: #imageLiteral(resourceName: "savingPrivateRyan")),
             3: Movie(id: 3, title: "Star Wars Episode I", genres: [Genres.SciFi], actors: ["Mark"], rating: 4, isFavourite: false, comments: "First prequel movie", image: #imageLiteral(resourceName: "starwarsI")),
             4: Movie(id: 4, title: "Star Wars Episode II", genres: [Genres.SciFi], actors: ["Mark"], rating: 4, isFavourite: false, comments: "Second prequel movie", image: #imageLiteral(resourceName: "starwarsII")),
-            5: Movie(id: 5, title: "Avatar", genres: [Genres.Action, Genres.SciFi], actors: ["Jane", "Jon", "Mary"], rating: 3, isFavourite: false, comments: "This is a really long test comment.  This is to test that multi-line comments work.", image: #imageLiteral(resourceName: "avatar"))
+            5: Movie(id: 5, title: "Avatar", genres: [Genres.Action, Genres.SciFi], actors: ["Jane", "Jon", "Mary"], rating: 3, isFavourite: false, comments: "This is a really long test comment.  This is to test that multi-line comments work.", image: #imageLiteral(resourceName: "avatar")),
+            6: Movie(id: 6, title: "testTitle2", genres: [Genres.Comedy], actors: ["Geoff", "Bob"], rating: 4, isFavourite: true, comments: "This is a good movie"),
+            7: Movie(id: 7, title: "testTitle3", genres: [Genres.Documentary], actors: ["Jack", "Bob"], rating: 2, isFavourite: true, comments: "This is a an ok movie")
         ]
         return collection
     }
