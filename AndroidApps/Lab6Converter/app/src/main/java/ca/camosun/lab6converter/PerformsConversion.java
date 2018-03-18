700a984db8c0367a9b0895469530c664242d46c5
Andrew Rozendal <ascrozendal@gmail.com>
2018-03-18 12:22:24 -0700

    updated comments for performsconversion interface

--- AndroidApps/Lab6Converter/app/src/main/java/ca/camosun/lab6converter/PerformsConversion.java
+++ AndroidApps/Lab6Converter/app/src/main/java/ca/camosun/lab6converter/PerformsConversion.java
@@ -1,6 +1,7 @@
 package ca.camosun.lab6converter;
 
-// Interface for all button conversion actions to conform to
+// Interface for all conversion actions to conform to
+// All conversion actions must convert an input Double to an output Double
 public interface PerformsConversion {
     Double convert(Double value);
 }
